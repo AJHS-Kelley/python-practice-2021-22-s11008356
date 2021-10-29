f02a3df486441fb9cc0327ddf0d7b6e3de504c4c
s11008356 <70557607+s11008356@users.noreply.github.com>
2021-10-29 09:21:41 -0400

    version 0.1
    Created integer and floating point variables

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Will Case, 10/29/21 09:06 am, version 0.0
+# Variables Practice, Will Case, 10/29/21 09:20 am, version 0.1
+
+# Three main types of variables for this class
+
+health = 345 # INTEGERS, any +/- whole number AND 0.
+account_balance = 12345.52 # FLOATING POINT, any +- with decimal.
+
+
