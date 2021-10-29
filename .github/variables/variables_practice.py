74731e5f6428c2311290daa66c67b4148ead339f
s11008356 <70557607+s11008356@users.noreply.github.com>
2021-10-29 09:38:17 -0400

    v 0.2
    printed health, account balance, and user name

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,8 +1,11 @@
-# Variables Practice, Will Case, 10/29/21 09:20 am, version 0.1
+# Variables Practice, Will Case, 10/29/21 09:37 am, version 0.2
 
 # Three main types of variables for this class
 
 health = 345 # INTEGERS, any +/- whole number AND 0.
 account_balance = 12345.52 # FLOATING POINT, any +- with decimal.
+user_name = "Mr. Spitemout" # STRING, letters/numbers printed on screen
 
-
+print(health) 
+print(account_balance)
+print(user_name)
